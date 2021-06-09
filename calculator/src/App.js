7faa62babc19763a2f5a803b0609983f2323b6c9
UDIT MEHRA <uditmehra80@gmail.com>
2021-06-09 16:37:57 +0530

    add toString

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -12,11 +12,11 @@ function App() {
   }
 
   function ClickOperation(e) {
-     if(result==""){
+     if(result===""){
        setResult(e.target.name)
-     }
+      }
      else{ 
-        if(result.charAt(result.length-1)==="+"){
+       if(result.charAt(result.length-1)==="+"){
              setResult(result.slice(0, -1) + e.target.name);
         }
         else if(result.charAt(result.length-1)==="-"){
@@ -29,9 +29,10 @@ function App() {
           setResult(result.slice(0, -1) + e.target.name);
         }
         else{
-          setResult(eval(result)  +  (e.target.name) );
+          setResult(eval(result).toString() + (e.target.name));
         }
-     }
+        
+    }
   }
 
   function backspace() {
@@ -44,7 +45,7 @@ function App() {
 
   function calculate() {
     try {
-      setResult(eval(result));
+      setResult(eval(result).toString());
     } catch (error) {
       setResult("Error");
     }
@@ -82,62 +83,25 @@ function App() {
       </form>
 
       <div className={darkMode ? "keypad-dark" : "keypad-light"}>
-        <button id="clear" onClick={clear}>
-         AC
-        </button>
-        <button id="backspace" onClick={backspace}>
-          C
-        </button>
-        <button name="+" onClick={ClickOperation}>
-          +
-        </button>
-        <button name="7" onClick={handleClick}>
-          7
-        </button>
-        <button name="8" onClick={handleClick}>
-          8
-        </button>
-        <button name="9" onClick={handleClick}>
-          9
-        </button>
-        <button name="-" onClick={ClickOperation}>
-          -
-        </button>
-        <button name="4" onClick={handleClick}>
-          4
-        </button>
-        <button name="5" onClick={handleClick}>
-          5
-        </button>
-        <button name="6" onClick={handleClick}>
-          6
-        </button>
-        <button name="*" onClick={ClickOperation}>
-          &times;
-        </button>
-        <button name="1" onClick={handleClick}>
-          1
-        </button>
-        <button name="2" onClick={handleClick}>
-          2
-        </button>
-        <button name="3" onClick={handleClick}>
-          3
-        </button>
-        <button name="/" onClick={ClickOperation}>
-          /
-        </button>
-        <button name="0" onClick={handleClick}>
-          0
-        </button>
-        <button name="." onClick={handleClick}>
-          .
-        </button>
-        <button id="result" onClick={calculate}>
-          =
-        </button>
+        <button id="clear" onClick={clear}> AC </button>
+        <button id="backspace" onClick={backspace}> C </button>
+        <button name="+" onClick={ClickOperation}> + </button>
+        <button name="7" onClick={handleClick}> 7 </button>
+        <button name="8" onClick={handleClick}> 8 </button>
+        <button name="9" onClick={handleClick}> 9 </button>
+        <button name="-" onClick={ClickOperation}> - </button>
+        <button name="4" onClick={handleClick}> 4 </button>
+        <button name="5" onClick={handleClick}>  5 </button>
+        <button name="6" onClick={handleClick}>  6 </button>
+        <button name="*" onClick={ClickOperation}> &times; </button>
+        <button name="1" onClick={handleClick}>  1 </button>
+        <button name="2" onClick={handleClick}> 2 </button>
+        <button name="3" onClick={handleClick}> 3 </button>
+        <button name="/" onClick={ClickOperation}> / </button>
+        <button name="0" onClick={handleClick}>  0  </button>
+        <button name="." onClick={handleClick}>  .  </button>
+        <button id="result" onClick={calculate}>  = </button>
       </div>
-
       <div className="middle">
           <button onClick={() => setShow(!show)}>Scientific Mode</button>
       </div>
